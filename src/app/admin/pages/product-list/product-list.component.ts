dbdbb8d1f50ab6e2d3415df751e4b7ae192421cd
David Stewart <david.stewartsb@gmail.com>
2022-09-17 13:44:39 -0300

    add: get on the table productListComponent

--- src/app/admin/pages/product-list/product-list.component.ts
+++ src/app/admin/pages/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Product, ProductsService, SweetAlertService } from '@util';
 
 @Component({
   selector: 'app-product-list',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./product-list.component.css']
 })
 export class ProductAdminListComponent implements OnInit {
-
-  constructor() { }
+  products: any = []
+  constructor(
+    private router: Router,
+    private productService: ProductsService,
+    private alertService: SweetAlertService
+    ) { }
 
   ngOnInit(): void {
+    this._getProducts()
   }
-
+  deleteProduct(product: Product){
+    this.productService.deleteProduct(product).subscribe({
+      next: () => this.alertService.successSwal('Produto deletado', 'Sucesso'),
+      error: () => this.alertService.errorSwal('Tente novamente mais tarde', 'Erro'),
+      complete: () => this._getProducts()
+    })
+  }
+  _getProducts(){
+    this.productService.getProducts().subscribe(products => {
+      this.products = products
+      console.log(products)
+    })
+  }
+  updateProduct(productId: string){
+    this.router.navigateByUrl(`produtos/formulario/${productId}`)
+  }
 }
