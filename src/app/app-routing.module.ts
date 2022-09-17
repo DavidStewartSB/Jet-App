321ed0ae0812c556d4ba234d48cd89359e624197
David Stewart <david.stewartsb@gmail.com>
2022-09-17 13:23:51 -0300

    add: routing for project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,32 @@
+import { ProductAdminFormComponent } from './admin/pages/product-form/product-form.component';
+import { ProductAdminListComponent } from './admin/pages/product-list/product-list.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductListComponent } from './store/pages/product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: ProductListComponent
+  },
+  {
+    path: 'todos-produtos',
+    component: ProductListComponent
+  },
+  {
+    path: 'produtos',
+    component: ProductAdminListComponent
+  },
+  {
+    path: 'produtos/formulario',
+    component: ProductAdminFormComponent
+  },
+  {
+    path: 'produtos/formulario/:id',
+    component: ProductAdminFormComponent
+  }
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
